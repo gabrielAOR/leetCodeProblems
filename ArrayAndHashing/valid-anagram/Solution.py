443255b5490f0d24c096c93a7dfd656c5695cd2b
Gabriel Alves <gaoralves@gmail.com>
2024-04-09 13:27:59 -0300

    Valid anagram new Solution

--- ArrayAndHashing/valid-anagram/Solution.py
+++ ArrayAndHashing/valid-anagram/Solution.py
@@ -1,5 +1,16 @@
 # Problem link: https://leetcode.com/problems/valid-anagram/description/
-
 class Solution(object):
     def isAnagram(self, s, t):
-      return sorted(s) == sorted(t)
+        if len(s) != len(t): 
+            return False
+
+        dictS, dictT = {}, {}
+
+        for i in range(len(s)):
+            dictS[s[i]] = 1 + dictS.get(s[i], 0)
+            dictT[t[i]] = 1 + dictT.get(t[i], 0)
+        for key in dictS:
+            if dictS[key] != dictT.get(key, 0): 
+                return False
+
+        return True

bea648d2e50222af1544c3f6a795a6c7b121c8c5
Gabriel Alves <gaoralves@gmail.com>
2024-04-10 13:19:59 -0300

    contains-duplicate Solution in python

---
+++ ArrayAndHashing/contains-duplicate/Solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashSet = set()
+        for num in nums:
+            if num in hashSet:
+                return True
+            hashSet.add(num)
+        return False
